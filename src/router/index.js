687f35773f1f5685c2f47b756dbd98e35195b5de
Mach <machkerman@gmail.com>
2021-01-12 05:57:27 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+const originalPush = VueRouter.prototype.push
+
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err)
+}
+const routes = [
+  {
+    path: '/',
+    name: 'HomePage',
+    component: () => import('@/views/HomePage/HomePage.vue'),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
---
+++ src/views/HomePage/HomePage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <button @click="add100List">生成新的D100</button>
+   <div v-for="item in rand100List" :key="item.id">{{item.id+"  |  "+item.data}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    props: {},
+    components: {},
+    directives: {}, 
+    mixins: [], 
+    data() {
+        return {
+          rand100List:[]
+        }
+    },
+    computed: {}, 
+    methods: { 
+      rand100(){
+        return Math.ceil(Math.random()*100)
+      },
+      add100List(){
+        this.rand100List.push({
+          id:this.rand100List[this.rand100List.length-1].id+1,
+          data:this.rand100()
+        })
+        if(this.rand100List.length>30){
+          this.rand100List.splice(0,1)
+        }
+      }
+    },
+    watch: {}, 
+    created() {
+      this.rand100List.push({
+          id:0,
+          data:this.rand100()
+        })
+        
+    } 
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
